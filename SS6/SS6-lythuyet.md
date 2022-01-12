e85e280a6a7ab8fc5c3e3558adc35ba91a4a3b77
hieu1966hn <hieu1966hn@gmail.com>
2022-01-12 19:37:46 +0700

    SS6

---
+++ SS6/SS6-lythuyet.md
@@ -0,0 +1,2 @@
+- Bài toán: Nhà hàng cần lưu trữ các món ăn online: (LocalStorage)
+- Lấy được dữ liệu từ LocalStorage và tiến hành hiển thị các dữ liệu trên trang web của mình với Javascript:
---
+++ SS6/data.js
@@ -0,0 +1,11 @@
+// file nay: Dùng để khai báo các món ăn dưới dạng mảng chứa đối tượng;
+let menu = [
+  {
+    name: "Pho",
+    title: "Day la mon an co truyen Ha Noi va Viet Nam",
+    des: "Mon pho ra doi vao ngay ,....Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, architecto autem numquam, tenetur quo natus dicta beatae in saepe nam distinctio ullam. Quam est reiciendis, iure repellendus in adipisci molestias?",
+    
+  },
+];
+
+console.log(menu);
