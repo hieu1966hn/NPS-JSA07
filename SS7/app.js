067218abf69d12d254c1c3cfb35df39df19d686a
hieu1966hn <hieu1966hn@gmail.com>
2022-01-19 20:09:52 +0700

    done ES6

--- SS7/app.js
+++ SS7/app.js
@@ -22,44 +22,45 @@ console.log("Pi: ", Pi);
 // var b = "world"
 // console.log(a + " " + b);
 
-/// VD: 
-{
-  {
-    // let cha = "Đây là biến được khai báo ở khối 2"
-    {
-      // console.log("cha: ", cha);
-      var con = "Đây là biến được khai báo ở khối 3"
+// /// VD: 
+// {
+//   {
+//     // let cha = "Đây là biến được khai báo ở khối 2"
+//     {
+//       // console.log("cha: ", cha);
+//       var con = "Đây là biến được khai báo ở khối 3"
 
-    }
-  }
-  // console.log("cha: ", cha); // Lỗi, vì không thuộc phạm vi khai báo
-}
-console.log("con: ", con);// in ra gì?
+//     }
+//   }
+//   // console.log("cha: ", cha); // Lỗi, vì không thuộc phạm vi khai báo
+// }
+// console.log("con: ", con);// in ra gì?
 // NX: khai báo biến với cú pháp var: đưa toàn bộ các biến được khai báo với cú pháp var cho toàn bộ file sử dụng
 // Lưu ý: Khi khai báo biến: nên khai báo bằng từ khóa "let".
 
 
 // 3. Arrow Function (hàm mũi tên)
+// Lưu ý: khi sau mũi tên là 1 câu lệnh. Hiểu nó là viết tắt của return
 // Cú pháp:
 // VD: khai báo hàm cộng 2 số bất kỳ: 
-function sum(a, b) {
-  return a + b
-}
+// function sum(a, b) {
+//   return a + b
+// }
 
-sum(1, 2);// 3
-console.log("sum(1,2): ", sum(1, 2));
+// sum(1, 2);// 3
+// console.log("sum(1,2): ", sum(1, 2));
 
-// C2: 
-let sum1 = function (a, b) {
-  return a + b
-}
-console.log("sum1(3,4): ", sum1(3, 4));
+// // C2: 
+// let sum1 = function (a, b) {
+//   return a + b
+// }
+// console.log("sum1(3,4): ", sum1(3, 4));
 
-// C3: Arrow function: nên dùng const
-const sum2 = (a, b) => {
-  return a + b;
-}
-console.log("sum2(5,5): ", sum2(5, 5));
+// // C3: Arrow function: nên dùng const
+// const sum2 = (a, b) => {
+//   return a + b;
+// }
+// console.log("sum2(5,5): ", sum2(5, 5));
 
 /*
 const tenHam = (parmeters,..) => {
@@ -67,4 +68,28 @@ const tenHam = (parmeters,..) => {
 }
 */
 
+/// VD: Arrow function
+// let arr = [1, 2, 3, 4, 5];
+// arr.forEach(i =>
+//   console.log(i + " => " + i * i)
+// )
+
+// VD: forEach
+// let arr = [1, 2, 3, 4, 5];
+// arr.forEach(i => console.log(i));
+
+
+
+////// 4. Các tham số mặc định (pending)
+// let hamX = (a, b = 1) => a * b;
+// console.log(hamX(2, 3)); // 2 * 1 = 2
+
+////// 5. Các tham số còn lại: 
+const X = (a, b, ...args) => {
+  console.log(`a = ${a}, b = ${b}, ... ${args.length} args left`);
+  console.log(args);
+}
+
+X(2, 3);// 2,3
+X(1, 2, 3, 4, 5)// 1,2 345
 
