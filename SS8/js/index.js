0e16c54f1d7c9370aa01f6638902337132689294
hieu1966hn <hieu1966hn@gmail.com>
2022-02-23 20:24:21 +0700

    done fetch dữ liệu

---
+++ SS8/js/index.js
@@ -0,0 +1,33 @@
+const APP_ID = '503628701a0938d24cc366e3125f41a1';
+
+const searchInput = document.getElementById("search-input");
+
+
+const cityName = document.querySelector(".city-name");
+const weatherState = document.querySelector(".weather-state");
+const weatherIcon = document.querySelector('.weather-icon');
+const temperature = document.querySelector(".temperature");
+
+const sunrise = document.querySelector(".sunrise");
+const sunset = document.querySelector(".sunset");
+
+const humidity = document.querySelector(".humidity")
+const windSpeed = document.querySelector(".wind-speed");
+
+
+
+searchInput.addEventListener("change", (event) => {
+  console.log(event.target.value);
+  // request dữ liệu tới server thông qua fetch
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${event.target.value}&appid=${APP_ID}&lang=vi`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data)
+
+      cityName.innerHTML = data.name;
+      weatherState.innerHTML = data.weather[0].description;
+
+    });
+
+
+})
